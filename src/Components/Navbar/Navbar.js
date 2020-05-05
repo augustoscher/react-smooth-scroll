1f95f1b4abe4314d10ca143af4f635fb1e4f5a2d
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-05 16:18:06 -0300

    Initial commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { Link, animateScroll as scroll } from "react-scroll";
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="nav" id="navbar">
+      <div className="nav-content">
+        <Link
+          className="navbar-brand"
+          activeClass="active"
+          to="section1"
+          spy={true}
+          smooth="easeInOutBack"
+          offset={-70}
+          duration={500}
+        >
+          <span className="navbar-brand-span">Augusto</span> Scher
+        </Link>
+        <ul className="nav-items">
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth="easeInOutBack"
+              offset={-70}
+              duration={500}
+            >
+              <span>Section 1</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section2"
+              spy={true}
+              smooth="easeInOutBack"
+              offset={-70}
+              duration={500}
+            >
+              <span>Section 2</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section3"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <span>Section 3</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section4"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <span>Section 4</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section5"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <span>Section 5</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/Components/Section.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section = ({ title, subtitle, dark, id }) => {
+  return (
+    <div className={"section" + (dark ? " section-dark" : "")}>
+      <div className="section-content" id={id}>
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
