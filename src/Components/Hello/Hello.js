8fcfd4275d57d732d3128f501c17d95439fd9176
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-06 12:50:06 -0300

    Hello page

--- src/Components/Hello/Hello.js
+++ src/Components/Hello/Hello.js
@@ -1,30 +1,49 @@
 import React from "react";
+import styled from 'styled-components';
 import "./Hello.scss";
 import Icon from "../../Assets/Icons/icon-mouse.svg";
 import ReactRotatingText from "react-rotating-text";
 
+const Content = styled.div`
+  margin-top: 100px;
+`;
+
+const CenterDiv = styled.div`
+  text-align: center;
+  margin-bottom: 100px;
+`;
+
+const Name = styled.h1`
+  font-size: 60px;
+  font-weight: 800;
+  color: var(--black);
+  margin: 0;
+`;
+
 const Hello = () => {
   return (
-    <div className="hello">
-      <div className="text-center">
+    <Content>
+      <CenterDiv>
         <span className="subheading" role="img" aria-label="Hello">
           👋 Eu sou
         </span>
-        <h1 className="name">Augusto Scher</h1>
+        <Name>Augusto Scher</Name>
         <div className="rotating-text">
           <ReactRotatingText
             typingInterval={50}
+            emptyPause={500}
+            pause={1000}
             color="#FF3465"
             items={["Software Engineer", "at", "Resultados Digitais"]}
           />
         </div>
-      </div>
+      </CenterDiv>
       <div className="mouse-icon">
         <div className="mouse-wheel">
           <img src={Icon} alt="mouse" />
         </div>
       </div>
-    </div>
+    </Content>
   );
 };
 
