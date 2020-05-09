8c6a8bf3257f14c6738f798ebdddb0fb9e1ded44
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-09 02:50:10 -0300

    internacionalization

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 import { Link } from "react-scroll";
 import SECTION_TYPES from "../../Constants/SectionType";
@@ -10,9 +10,37 @@ const NavItem = styled.div`
   padding: 8px;
 `;
 
+const items = [
+  { sectionId: SECTION_TYPES.HELLO, translationKey: "navbar:welcome" },
+  { sectionId: SECTION_TYPES.ABOUT, translationKey: "navbar:about" },
+  { sectionId: SECTION_TYPES.CURRICULUM, translationKey: "navbar:curriculum" },
+  { sectionId: SECTION_TYPES.CONTACT, translationKey: "navbar:contact" },
+];
+
+const renderNav = (t) => {
+  return items.map((value, idx) => {
+    const text = t(value.translationKey);
+
+    return (
+      <li key={idx} className="nav-item">
+        <Link
+          activeClass="active"
+          to={value.sectionId}
+          spy={true}
+          smooth={true}
+          offset={-70}
+          duration={500}
+        >
+          <NavItem>{text}</NavItem>
+        </Link>
+      </li>
+    );
+  });
+};
+
 const Navbar = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const { t } = useTranslation(['translation', 'navbar']);
+  const { t } = useTranslation(["translation", "navbar"]);
 
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-light" id="navbar">
@@ -40,60 +68,11 @@ const Navbar = () => {
             collapsed ? "collapse navbar-collapse" : "navbar-collapse"
           }`}
         >
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to={SECTION_TYPES.HELLO}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <NavItem>{t('navbar:welcome')}</NavItem>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to={SECTION_TYPES.ABOUT}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <NavItem>{t('navbar:about')}</NavItem>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to={SECTION_TYPES.CURRICULUM}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <NavItem>{t('navbar:curriculum')}</NavItem>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                activeClass="active"
-                to={SECTION_TYPES.CONTACT}
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                <NavItem>{t('navbar:contact')}</NavItem>
-              </Link>
-            </li>
-          </ul>
+          <ul className="navbar-nav">{renderNav(t)}</ul>
         </div>
       </div>
     </nav>
   );
 };
 
-export default (Navbar);
+export default Navbar;
