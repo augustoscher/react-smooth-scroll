b6f973f7a3afdf79ccecc65eb072b8fa1d3bf142
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-10 14:54:39 -0300

    About internacionalization

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import dummyText from "../DummyText";
 
 const Contact = () => {
   return (
     <>
       <h1>Contact</h1>
-      <p>{dummyText}</p>
+      <p>Oi, tudo bem?</p>
     </>
   );
 }
