d4dcafc8a8d4de740ae2c3a638715a656834f334
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-13 15:42:08 -0300

    Curricullum component

--- src/Components/Section/SectionCurriculum/SectionCurriculum.js
+++ src/Components/Section/SectionCurriculum/SectionCurriculum.js
@@ -1,16 +1,21 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 import componentRelations from "../componentsRelations";
 
+const Content = styled.div`
+  // max-width: 1000px;
+  margin: 0 auto;
+  padding: 40px 20px;
+`;
 
 const SectionCurriculum = ({ id, colored }) => {
   const Component = componentRelations[id];
   return (
     <div className={"section" + (colored ? " section-colored" : "")}>
-      <div id={id}>
+      <Content id={id}>
         <Component />
-      </div>
+      </Content>
     </div>
   );
 };
