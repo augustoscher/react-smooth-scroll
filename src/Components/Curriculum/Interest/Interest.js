568d83c5e78a11aee267629be3156124d597462d
Augusto S. Scher <augusto.scher@resultadosdigitais.com.br>
2020-05-18 23:31:28 -0300

    Interest Component

--- src/Components/Curriculum/Interest/Interest.js
+++ src/Components/Curriculum/Interest/Interest.js
@@ -1,23 +1,26 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 import InterestCard from './InterestCard/InterestCard';
 
-// const Content = styled.div`
-  // padding: 30px;
-// `;
+const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: .5rem!important;
+  margin-right: 5px!important;
+`;
 
 const Interest = ({ title }) => (
   <>
     <h1>{title}</h1>
-    {/* <p>Tenho interesse, principalmente em stacks como:</p> */}
-    <div className="row mb-2">
+    <p>Tenho interesse, principalmente em stacks como:</p>
+    <Content className="row mb-2">
       <InterestCard title="Node.js" />
       <InterestCard title="Golang" />
       <InterestCard title="Python" />
       <InterestCard title="React.js" />
       <InterestCard title="Data Science" />
       <InterestCard title="Fullstack" />
-    </div>
+    </Content>
   </>
 );
 
